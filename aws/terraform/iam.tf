cde2cbad19131b6d6a27f8a02605c8796d0824c4
koki-develop <kou.pg.0131@gmail.com>
2022-11-25 10:39:07 +0900

    iam role を作成

--- aws/terraform/iam.tf
+++ aws/terraform/iam.tf
@@ -11,3 +11,29 @@ resource "aws_iam_openid_connect_provider" "circleci" {
   client_id_list  = [var.circleci_organization_id]
   thumbprint_list = data.tls_certificate.circleci.certificates[*].sha1_fingerprint
 }
+
+data "aws_iam_policy_document" "circleci_assume_role_policy" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.circleci.arn]
+    }
+    condition {
+      test     = "StringLike"
+      variable = "oidc.circleci.com/org/${var.circleci_organization_id}:sub"
+      values   = ["org/${var.circleci_organization_id}/project/${var.circleci_project_id}/user/*"]
+    }
+  }
+}
+
+resource "aws_iam_role" "circleci" {
+  name               = "circleci-oidc-example-role"
+  assume_role_policy = data.aws_iam_policy_document.circleci_assume_role_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "circleci_s3_readonly" {
+  role       = aws_iam_role.circleci.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
